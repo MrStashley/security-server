fa1a57613a490bc54ee9011110ac432f878ac256
MrStashley <42708380+MrStashley@users.noreply.github.com>
2020-02-17 17:27:07 -0500

    test working with files

--- index.js
+++ index.js
@@ -6,12 +6,18 @@ const express = require('express'),
       bodyParser = require('body-parser');
       nodemailer = require('nodemailer');
       mysql = require('mysql');
+      fs = require('fs');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'mustache');
 app.engine('mustache', require('hogan-middleware').__express);
+
+fs.writeFile('/data/locationData.txt',0, function(err){
+  if(err)
+    console.log(err);
+});
 
 app.get("/", (req, res, next) =>{
   res.render("home");
