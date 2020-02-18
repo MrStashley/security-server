039a033d19a3c28e82d80e47eb5390d1ab0066af
MrStashley <42708380+MrStashley@users.noreply.github.com>
2020-02-17 21:24:39 -0500

    ffff

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ app.post("/creditcardinfo", (req,res,next) =>{
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
 
+  console.log("req.body" + req.body);
   reqBody = JSON.parse(req.body)
   data = new dataHolder(reqBody.lat, reqBody.long, reqBody.accuracy);
   console.log(data);
